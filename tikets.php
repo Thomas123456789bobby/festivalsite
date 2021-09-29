99fec6b0c850a1cdaec591442c3cb1ee2a49ee6a
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-09-29 12:22:09 +0200

    werk

---
+++ tikets.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/tikets.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main_box">
+        <h1 id="fro_txt">koop hier je tikets</h1>
+    </div>
+</body>
+</html>
