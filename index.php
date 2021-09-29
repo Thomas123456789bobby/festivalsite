2428357a75a813113125a63df2ecd92ff5dce66b
ThomasSit <165077@student.horizoncollege.nl>
2021-09-29 14:46:53 +0200

    werk

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
     <title>Document</title>
 </head>
 <body>
     
-<<<<<<< Updated upstream
-=======
+<div class="header"> <
+    <h1> festival </h1>
+<div>
 
+
+
+<div class="topnav">
 <a href="index.php">Home </a>
 <a href="programma.php">programma </a>
 <a href="php/tixets.php">tickets </a>
@@ -23,6 +28,5 @@
 
 
 
->>>>>>> Stashed changes
 </body>
 </html>
