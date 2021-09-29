5ad193847f5fb1d0426c2e610e0e19f2070381f7
maher170 <90043231+maher170@users.noreply.github.com>
2021-09-29 12:46:58 +0200

    werk

---
+++ tijden.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>programma</h2>
+
+    <table>
+        <div>
+            <li>naam optreden</li>
+            <li>naam poduim</li>
+            <li>tijd</li>
+        </div>
+        <div>
+            <li>boef</li>
+            <li></li>
+            <li></li>
+
+        </div>
+
+
+
+
+    </table>
+
+
+
+</body>
+
+</html>
