55c2e0b23f822ee37966527a3d89ef1bb46728db
maher170 <90043231+maher170@users.noreply.github.com>
2021-10-06 09:28:51 +0200

    werk

--- tijden.php
+++ tijden.php
@@ -5,29 +5,158 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title> 
+
+    <link rel="stylesheet"  href="css/tijden.css"> 
+
+
 </head>
 
+
+
+
 <body>
-    <h2>programma</h2>
 
-    <table>
-        <div>
-            <li>naam optreden</li>
-            <li>naam poduim</li>
-            <li>tijd</li>
-        </div>
-        <div>
-            <li>boef</li>
-            <li></li>
-            <li></li>
+<div class="container">
 
-        </div>
+    <ul class="navbar">
+        <li>home</li>
+        <li>programma</li>
+        <li>tickets</li>
+
+    </ul>
+    <h1 id="hoofd">Noorderfestival</h1> 
+
+</div>
 
 
 
+<div class="maher">
 
+<table>
+
+    <h2>zaterdag</h2>
+    
+        <thead>
+        
+        <tr>
+            <td>optreden</td>
+            <td> poduim</td>
+            <td>tijd</td>
+            <tr>
+        </thead>
+
+
+
+        <tbody>
+            <td>boef</td>
+            <td>poduim A1</td>
+            <td>10.00</td>
+
+            <tr>
+                <td>lil klein</td>
+                <td>A2</td>
+                <td>12.00</td>
+            </tr>
+                 
+            <tr>
+                <td>sevn alias</td>
+                <td>poduim 1B</td>
+                <td>13.00</td>
+            </tr>
+        
+            <tr>
+                <td>josilvio</td>
+                <td>poduim 2B</td>
+                <td>14.00</td>
+            </tr>
+                
+            <tr>
+                <td>lijpe</td>
+                <td>poduim 1C</td>
+                <td>15.00</td>
+            </tr>
+            <tr>
+                <td>ismo</td>
+                <td>2C</td>
+                <td>16.00</td>
+            </tr>
+            
+            </tbody>
+
+        
     </table>
+</div>
+
+<div class="maher">
+<h2>zondag</h2>
+   <table>
+   
+    <thead>
+        <th>optreden</th>
+        <th> poduim</th>
+        <th>tijd</th>
+
+    </thead>
+
+
+
+    <tbody>
+        <td>lil nax</td>
+        <td>poduim 3C</td>
+        <td>12.00</td>
+
+        <tr>
+            <td>mensa</td>
+            <td>C4</td>
+            <td>13.00</td>
+        </tr>
+
+        <tr>
+            <td>bizzy</td>
+            <td>A1</td>
+            <td>14.00</td>
+        </tr>
+        <tr>
+            <td>keizer</td>
+            <td>2A</td>
+            <td>15.00</td>
+        </tr>
+        <tr>
+            <td>cucee</td>
+            <td>1B</td>
+            <td>17.00</td> 
+
+        </tr>
+        <tr>
+            <td>nessim</td>
+            <td>2B</td>
+            <td>18.00</td>
+        </tr>
+        
+
+        </tbody>
+ </div>
+
+</table>
+<div class="figure">
+
+
+</div>
+
+<footer>
+
+<img id="img" src="img/telefoon.jpg" width="50px" height="50px" > 
+<p>06 28857241</p>
+
+<img id="email" src="img/R.png" width="50px" height="50px" >
+<p>165077@student.hor</p>
+
+</footer>
+
+
+
+
 
 
 
